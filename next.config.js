614b7e6fc7780fd911f0560b2738fd235d7fd6a3
jigglypop <ydh2244@gmail.com>
2023-11-23 02:48:39 +0900

    1123

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-// import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 const withVanillaExtract = createVanillaExtractPlugin({});
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  output: 'export',
   reactStrictMode: true,
   images: {
     remotePatterns: [
